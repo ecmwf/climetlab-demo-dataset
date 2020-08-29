e9f5f03b780df67521ff4d39662c21a0408a3428
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2020-08-29 12:50:28 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Empty
+# climetlab-demo-dataset
---
+++ setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+#
+# Copyright 2017-2018 European Centre for Medium-Range Weather Forecasts (ECMWF).
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import io
+import os
+
+import setuptools
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return io.open(file_path, encoding="utf-8").read()
+
+
+version = "0.0.1"
+
+
+setuptools.setup(
+    name="climetlab-demo-dataset",
+    version=version,
+    description="Example climetlab external dataset plugin",
+    long_description=read("README.md"),
+    author="European Centre for Medium-Range Weather Forecasts (ECMWF)",
+    author_email="software.support@ecmwf.int",
+    license="Apache License Version 2.0",
+    url="https://github.com/ecmwf/climetlab-demo-dataset",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    zip_safe=True,
+    entry_points={'climetlab.datasets': ['demo-dataset = climetlab_demo_dataset']},
+    keywords="meteorology",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Operating System :: OS Independent",
+    ],
+)
