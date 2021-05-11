c887ce34b50628e1d8c38c855f1eb0be76dd600e
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2021-05-11 15:20:38 +0100

    Update

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def read(fname):
     return io.open(file_path, encoding="utf-8").read()
 
 
-version = "0.0.8"
+version = "0.0.9"
 
 
 setuptools.setup(
@@ -36,7 +36,9 @@ setuptools.setup(
     include_package_data=True,
     install_requires=["xarray"],
     zip_safe=True,
-    entry_points={"climetlab.datasets": ["demo-dataset = climetlab_demo_dataset"]},
+    entry_points={
+        "climetlab.datasets": ["demo-dataset = climetlab_demo_dataset:DemoDataset"]
+    },
     keywords="meteorology",
     classifiers=[
         "Development Status :: 3 - Alpha",
